66e45840ef24b1eee73f79ad9cbb1169e14684dd
Mallory Feuer <mallory.feuer@gmail.com>
2018-03-19 18:09:23 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -53,7 +53,7 @@ class Dog
       SQL
       
       DB[:conn].execute(sql, self.name, self.breed)
-      @id = DB[:conn].execute("SELECT last_insert_")
+      @id = DB[:conn].execute("SELECT last_insert_rowid()")
     end
   end
 end
